21022f03b81cee72bf01fd928f9a3b66c4c253e6
trangml <mattluutrang@vt.edu>
2023-06-28 20:44:24 -0400

    update GA ID and packages

--- src/components/utils/api.js
+++ src/components/utils/api.js
@@ -1,41 +1,27 @@
 // utils/api.js
 import fm from 'front-matter';
 
-// import raw from 'raw.macro';
-// import path from 'path';
+export const getBlogPosts = async () => {
+  const response = await fetch('/data/blog/');
+  const fileNames = await response.text();
+  console.log(fileNames);
 
-// const blogsDirectory = '../data/blog';
-const fileName = 'thoughts_on_ethics.md';
-// const fp = blogsDirectory + '/' + fileName;
-const temp1 = await fetch(
-  '/data/blog/thoughts_on_ethics.md',
-);
-const temp2 = await temp1.text();
+  const blogPosts = await Promise.all(
+    fileNames.map(async (fileName) => {
+      const files = await fetch(`/data/blog/${fileName}`);
+      const fileContent = await files.text();
+      const { attributes, body } = fm(fileContent);
+      const blogPost = {
+        id: fileName.replace(/\.md$/, ''), // Remove the '.md' extension from the file name
+        title: attributes.title,
+        body,
+        date: attributes.date,
+      };
+      return blogPost;
+    }),
+  );
 
-const getBlogPosts = async () => {
-  // const fileNames = fs.readdirSync(blogsDirectory);
-  const posts = [];
-  const { attributes, body } = fm(temp2);
-  posts.push({
-    id: fileName.replace(/\.md$/, ''), // Remove the '.md' extension from the file name
-    title: attributes.title,
-    body,
-    date: attributes.date,
-  });
-  // for (const fileName of fileNames) {
-  //   const filePath = path.join(blogsDirectory, fileName);
-  //   // const fileContent = fs.readFileSync(filePath, 'utf8');
-  //   const { data, content } = matter(fileContent);
-
-  //   posts.push({
-  //     id: fileName.replace(/\.md$/, ''), // Remove the '.md' extension from the file name
-  //     title: data.title,
-  //     content,
-  //     date: data.date
-  //   });
-  // }
-
-  return posts;
+  return blogPosts;
 };
 
 export default getBlogPosts;
