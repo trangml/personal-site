87161363e462d2271184cb3297a4f3b7fbe778f1
trangml <mattluutrang@vt.edu>
2023-09-03 01:02:06 -0400

    fix eslint string complaint

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -41,7 +41,7 @@ describe('renders the app', () => {
   });
 
   it('should render the title', async () => {
-    expect(document.title).toBe("Matthew Trang");
+    expect(document.title).toBe('Matthew Trang');
   });
 
   it('can navigate to /about', async () => {
